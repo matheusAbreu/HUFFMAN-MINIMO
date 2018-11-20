a9c3c0a39b0fc2981f942b7db48266e066a728c7
Guilherme Ennes <jaksonguilherme2@gmail.com>
2018-11-20 19:03:26 -0200

    implementação de função recolheDados de forma dinâmica

--- Algoritmo_Huffman_ED2/main.c
+++ Algoritmo_Huffman_ED2/main.c
@@ -6,26 +6,19 @@ int main()
 {
     int b= 4,i, c;
     char *vetor;
-    FILE *arquivo = fopen("arquivo.txt", "a+");
+    FILE *arquivo = fopen("arquivo.txt", "r");
+
+    fseek( arquivo, 0L, SEEK_END );
+    long tam_arquivo = ftell(arquivo);// Devolve numero de bytes do arquivo
+    rewind(arquivo);//retorna pro inicio do arquivo
+
+    fflush(arquivo);//avaliar necessidade dessa parte
+    vetor= (char*) malloc((tam_arquivo) * sizeof(char));
+    i=fread(vetor, sizeof(char), tam, arquivo);  //retorna o numero de bytes lidos alem de registrar os dados no vetor
+    if(tam_arquivo!= i)  //CORREÇÃO DOS BYTES ADICIONAIS OCASIONADOS PELA QUEBRA DE LINHA
+        vetor= (char*)realloc( vetor, i * sizeof(char));
+
+    vetor[i] = '\0';//força a delimitação da string
 
 
-    vetor= (char*) malloc(sizeof(char));
-
-    while(vetor[i]!= EOF)
-    {
-        vetor[i]= fgetc(arquivo);
-        if(vetor[i]!= EOF)
-            vetor= (char*) realloc(vetor, (++i)* sizeof(char));
-        i++;
-    }
-
-    printf("%s", vetor);
-
-
-
-        b=strlen(arquivo);
-
-    printf("%s", vetor);
-
-    system("pause");
 }
